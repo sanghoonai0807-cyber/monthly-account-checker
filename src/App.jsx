61aed418826a3ff010af68d313eac74e790782b0
sanghoonai0807-cyber <sanghoonai0807@gmail.com>
2026-03-25 21:09:08 +0900

    Create src/App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>청지기 금융 가계부</h1>
+      <p>앱이 정상적으로 로드되었습니다!</p>
+    </div>
+  )
+}
