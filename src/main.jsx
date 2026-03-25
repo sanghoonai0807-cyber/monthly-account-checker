129c8c0a0cd61b595ff49413516a2de1929754d5
sanghoonai0807-cyber <sanghoonai0807@gmail.com>
2026-03-25 21:13:25 +0900

    Update src/main.jsx to include React rendering code with CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
 
-// other existing code in src/main.jsx
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
